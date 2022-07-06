1601e4ffd79c39f196d5fd2e992f8c9e79232aa8
etinosaizekor <67836759+etinosaizekor@users.noreply.github.com>
2022-07-06 12:33:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,13 +12,11 @@ app.use(bodyParser.json())
 app.get('/:id', async (req, res)=>{
     const record_id = req.params.id
     const record = await Record.findAll({where: {id: record_id}})
-    console.log(record)
     res.send(JSON.stringify(record, null, 2))
 })
 
 app.post('/', (req, res)=> {
     const {company_name, email, CEO} = req.body
-    console.log(company_name, email, CEO)
     try{
         const newRecord = Record.create({company_name: company_name, official_email: email, CEO:CEO})
     } catch(err){
